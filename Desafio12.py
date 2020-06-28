c90a5e48396de5966a6af8a1c376f5eec6afdbd7
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-28 17:03:22 -0300

    Desafio 12 OK

---
+++ Desafio12.py
@@ -0,0 +1,6 @@
+print('[-- Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto. --]\n')
+preço = float(input('Digite o preço do produto: '))
+novopreço = preço*0.95
+print(f'O novo preço do produto é {novopreço:.2f} ')
+
+
