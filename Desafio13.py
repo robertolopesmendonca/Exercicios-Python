eba50259b1e90168db9125dedddf8567eaae04b7
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-28 17:12:13 -0300

    Desafio 13 OK !!

---
+++ Desafio13.py
@@ -0,0 +1,5 @@
+print('[-- Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento. --]\n')
+salario = float(input('Digite o seu salário: '))
+novosalario = salario*1.15
+print(f'O novo salário é {novosalario:.2f}')
+
