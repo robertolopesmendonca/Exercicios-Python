673a4df5e884a07f1852dd9bca8ea9ba5db83d0f
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-28 15:36:12 -0300

    Desafio 07 OK

---
+++ Desafio07.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2)/2
+
+print(f'Sua primeira nota foi {n1}, \nSua segunda nota foi {n2}, \ne sua média foi {media}')
