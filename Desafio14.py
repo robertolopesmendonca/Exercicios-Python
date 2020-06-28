0e00c82253cf1889b0e1ff54f8a860017c5c99ad
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-28 17:18:27 -0300

    Desafio 14 OK !!!

---
+++ Desafio14.py
@@ -0,0 +1,7 @@
+print('[-- Escreva um programa que converta uma temperatura digitada em °C e converta para °F. --]\n')
+
+celsius = float(input('Digite a temperatura em Celsius: '))
+fahrenheit = ((9*celsius)/5)+32
+print(f'A temperatura em Fahrenheit é {fahrenheit}')
+
+
