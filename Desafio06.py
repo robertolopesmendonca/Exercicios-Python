5679f50e4c3456fd15ba4b4ef0f6204682e5e8b7
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-28 15:30:08 -0300

    Desafio 07 OK !!

---
+++ Desafio06.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+dobro = 2*numero
+triplo = 3*numero
+raizquadrada = numero**(1/2)
+print(f'O número lido foi {numero} \n seu dobro foi {dobro}, \n seu triplo foi {triplo}, \n e sua raíz quadrada foi {raizquadrada}')
