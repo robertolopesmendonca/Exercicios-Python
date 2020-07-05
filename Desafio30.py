e457e3dcaee57e9102e40be8584ad2a3c7e59b28
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-07-05 11:21:01 -0300

    Desafio 30 OK !!

---
+++ Desafio30.py
@@ -0,0 +1,8 @@
+print("""
+030) Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+""")
+numero = int(input('Digite um número: '))
+if numero%2 == 0:
+    print('O número é par')
+else:
+    print('O número é impar')
