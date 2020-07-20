40e1691c3c7f404e4e5616fcb0dbb850549366a8
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-07-20 18:46:28 -0300

    Desafio 46 OK !!

---
+++ Desafio46.py
@@ -0,0 +1,12 @@
+print("""
+046) Faça um programa que mostre na tela uma contagem regressiva para o estouro de
+fogos de artifício, indo de 10 até 0, com pausa de 1 segundo entre eles.
+""")
+from time import sleep
+for c in range (10, -1, -1):
+    sleep(1)
+    print(c)
+print('BUMMMMM')
+
+
+
