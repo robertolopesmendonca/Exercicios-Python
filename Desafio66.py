6452b8622640f926dc0b2ca2bdc4848a6eda3d3d
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-08-03 20:44:11 -0300

    Desafio 66 OK !!

---
+++ Desafio66.py
@@ -0,0 +1,15 @@
+print("""
+066) Crie um programa que leia vários números inteiros pelo teclado. O programa só
+vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final
+mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando
+o flag).
+""")
+
+cont = soma = 0
+while True:
+    numero = int(input("Digite o número: "))
+    if numero == 999:
+        break
+    cont += 1
+    soma += numero
+print(f'{cont} números foram digitados, e a soma entre eles é {soma}') 
