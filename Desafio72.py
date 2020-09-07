c862ecb85afb42fb6007335ffb6c67b4e6705d5a
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-09-07 16:52:42 -0300

    72 e 73 OK !!

---
+++ Desafio72.py
@@ -0,0 +1,16 @@
+print("""
+072) Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, 
+de zero até vinte.
+Seu programa deverá ler um número pelo teclado(entre 0 e 20) e 
+mostrá-lo por extenso.
+""")
+contagem = ('zero', 'um', 'dois', 'três', 'quatro',
+'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 
+'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 
+'dezoito', 'dezenove', 'vinte')
+while True:
+    número = int(input('Digite um  número entre 0 e 20: '))
+    if 0 <= número <=20:
+        break
+    print('Tente novamente ', end= '')
+print('Você digitou o número {}'.format(contagem[número]))
